17d9af7d13329e87c4987b1e96a8fd5d0b70def1
vedantchoubey098 <49265501+vedantchoubey098@users.noreply.github.com>
2021-03-08 16:31:56 +0530

    changing raw date format to valid date format

--- src/components/table.js
+++ src/components/table.js
@@ -15,13 +15,29 @@ const Table = ({ data }) => {
               columns.map((heading) => <div className="Head">{heading}</div>)}
           </div>
     
-          {data.map((row) => (
+          {/* {data.map((row) => (
             <div className="Row">
               {columns.map((column) => (
                 <div className="Cell">{row[column]}</div>
               ))}
             </div>
-          ))}
+          ))} */}
+          {
+  data.map(row => {
+    var date = new Date( parseInt(row.date) * 1000).toLocaleString()
+    
+    return (
+      <div className="Row">
+        <div className="Cell">{row.id}</div>
+        <div className="Cell">{row.firstName}</div>
+        <div className="Cell">{row.lastName}</div>
+        <div className="Cell">{row.location}</div>
+        <div className="Cell">{date}</div>
+        <div className="Cell">{row.salary}</div>
+      </div>
+    )
+  })
+}
         </div>
       );
      
